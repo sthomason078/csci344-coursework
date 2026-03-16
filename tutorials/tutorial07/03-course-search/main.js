5321bdb6f568afe6133c090cb25f7b70f666504c
Stephen <sthomason@protonmail.com>
2026-03-16 16:35:49 -0400

    Update tutorial 7

--- tutorials/tutorial07/03-course-search/main.js
+++ tutorials/tutorial07/03-course-search/main.js
@@ -25,7 +25,7 @@ function doesTermMatch(course) {
     const code = course.Code.toLowerCase();
     const title = course.Title.toLowerCase();
     const CRN = course.CRN.toString();
-    const instructors = course.Instructors.map(instructor => instructor.Name).join("").toLowerCase();
+    const instructors = course.Instructors.map(instructor => instructor.Name).join(" ").toLowerCase();
 
     return (
         code.includes(searchTerm) ||
@@ -38,18 +38,30 @@ function doesTermMatch(course) {
 function dataToHTML(course) {
     // should return a formatted HTML card with the relevant course info
     // (using template literals).
+
+    // do not display data when the value is null
+    const days = course.Days !== null ? `${course.Days} &bull; ` : "";
+    const location = course.Location.FullLocation !== null ? `${course.Location.FullLocation} &bull; ` : "";
+
+    const seatsAvailable = () => {
+        if (isClassFull(course))
+            return `Number on Waitlist: ${course.WaitlistAvailable}`;
+        else
+            return `Seats Available: ${course.EnrollmentMax - course.EnrollmentCurrent}`;
+    }
+
     return `
         <section class="course-card">
-            <h2>CSCI 183.001: Intro to Programming: Data Science</h2>
-            <p class="status open">
-                <i class="fa-solid fa-circle-check"></i>
-                Open &bull; 10320 &bull; Seats Available: 14
+            <h2>${course.Code}: ${course.Title}</h2>
+            <p class="status ${isClassFull(course) ? "closed" : "open"}">
+                <i class="fa-solid ${isClassFull(course) ? "fa-circle-xmark" : "fa-circle-check"}"></i>
+                ${isClassFull(course) ? "Closed" : "Open"} &bull; ${course.CRN} &bull; ${seatsAvailable()}
             </p>
             <p>
-                MWF &bull; ZEI 201&bull; 3 credit hour(s)
+                ${days}${location}${course.Hours} credit hour${course.Hours === 1 ? "" : "s"}
             </p>
             <p>
-                <strong>Sarris, Michael</strong>
+                <strong>${course.Instructors.map(instructor => instructor.Name).join(" &bull; ")}</strong>
             </p>
         </section>
     `;
@@ -58,17 +70,30 @@ function dataToHTML(course) {
 // 3. Main Functions
 function showMatchingCourses() {
     // 1. Get the .courses container element
+    const coursesEl = document.querySelector(".courses");
     // 2. Clear it
+    coursesEl.innerHTML = "";
     // 3. Start with courseList (from course-data.js)
     // 4. Apply the filters and store the matched courses in a variable
+    const filteredCourseList = courseList.filter(course => doesTermMatch(course) && (openOnly ? !isClassFull(course) : true));
     // 5. If no courses match, display "No courses match your search." and return
+    if (filteredCourseList.length === 0) {
+        coursesEl.innerHTML = "No courses match your search.";
+        return;
+    }
     // 6. Output each course to the .courses container (forEach + insertAdjacentHTML)
+    filteredCourseList.forEach(course => {
+        coursesEl.insertAdjacentHTML("beforeend", dataToHTML(course));
+    });
 }
 
 function filterCourses() {
     // Update global variables (searchTerm and openOnly) by
     // reaching into the DOM and retrieving their values
+    searchTerm = document.querySelector("#search_term").value;
+    openOnly = document.querySelector("#is_open").checked;
     // Invoke the showMatchingCourses() function
+    showMatchingCourses();
 }
 
 // 4. Initial Render
